00f64ec1da4402823fc49be16b36d1cc137ad158
DeadEndjke <perceptionarmy@icloud.com>
2022-12-27 23:22:51 +0700

    final

---
+++ DataModels/Entities/History.cs
@@ -0,0 +1,26 @@
+﻿using CalculationsModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels.Entities
+{
+    public class History : EntityBase
+    {
+        public string FirstOperand { get; set; } = "";
+        public string Operation { get; set; } = "";
+        public string Result { get; set; } = "";
+        public string? SecondOperand { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        public override string ToString()
+        {
+            return string.IsNullOrWhiteSpace(SecondOperand)
+                ? $"{Operation}({FirstOperand}) = {Result}"
+                : $"{FirstOperand} {Operation} {SecondOperand} = {Result}";
+        }
+    }
+}
